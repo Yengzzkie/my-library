ef9d7cb986416c698ae77a767ae8397450d1ae5e
Yengzzkie <gatchalian.manuel@gmail.com>
2024-02-19 16:22:26 -0500

    style/added styling and images

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let myLibrary = [
-    // { id: 1, title: "Hobbit", author: "JK Rowling", pages: "185", isRead: "Read" }
+    { id: 1, title: "Hobbit", author: "JK Rowling", pages: "185", isRead: "Read" }
   ];
   
   // Object constructor
@@ -10,9 +10,24 @@ let myLibrary = [
     this.isRead = isRead;
   }
   
+  // opens the input modal
+  const modal = document.querySelector(".modal");
+  const addBookBtn = document
+    .querySelector(".add-book-btn")
+    .addEventListener("click", () => {
+      modal.showModal();
+  
+      const closeModalBtn = document
+        .querySelector(".close-modal")
+        .addEventListener("click", () => {
+          modal.close();
+        });
+    });
+  
   // Main function to add the book to the array
-  const submitBtn = document.getElementById("submitButton").addEventListener("click", function addBook(event) {
-
+  const submitBtn = document
+    .getElementById("submitButton")
+    .addEventListener("click", (event) => {
       event.preventDefault();
   
       const inputTitle = document.getElementById("title").value;
@@ -20,11 +35,16 @@ let myLibrary = [
       const inputPages = document.getElementById("pages").value;
       const inputRead = document.getElementById("isRead").checked;
   
-      const newBook = new Book(inputTitle, inputAuthor, inputPages, inputRead ? "Read" : "Not read");
-      
+      const newBook = new Book(
+        inputTitle,
+        inputAuthor,
+        inputPages,
+        inputRead ? "Read" : "Not read"
+      );
+  
       myLibrary.push(newBook);
-      saveToLocalStorage();
-      displayBook();
+      console.log(myLibrary);
+      renderLibrary();
   
       //Clears the input field upon submission and close the modal
       document.getElementById("title").value = "";
@@ -33,8 +53,8 @@ let myLibrary = [
       modal.close();
     });
   
-  // Displays the book information to the DOM
-  function displayBook() {
+  // renders the book object to the DOM
+  function renderLibrary() {
     const objContainer = document.querySelector(".objContainer");
   
     objContainer.innerHTML = "";
@@ -43,69 +63,42 @@ let myLibrary = [
       const bookObj = document.createElement("div");
       const p = document.createElement("p");
       const deleteBtn = document.createElement("button");
-      
-
-      objContainer.appendChild(bookObj);
-      bookObj.appendChild(p);
-      bookObj.appendChild(deleteBtn);
   
       bookObj.classList.add("book");
       p.style.whiteSpace = "pre-line";
-      p.textContent = `Index: ${index} \nTitle: ${book.title}\nAuthor: ${book.author} \nPages: ${book.pages} \nStatus: ${book.isRead}`;
-      deleteBtn.textContent = "Delete";
-    });
-  }
+      p.textContent = `ID: ${index + 1} \nTitle: ${book.title}\nAuthor: ${
+        book.author
+      } \nPages: ${book.pages} \nStatus: ${book.isRead}`;
+      deleteBtn.classList.add("delete-book");
+      deleteBtn.textContent = "X";
 
-  document.querySelector(".objContainer").addEventListener("click", function (event) {
-    if (event.target.tagName === "BUTTON" && event.target.textContent === "Delete") {
-      const bookModal = document.querySelector(".book-modal");
-      const bookObj = event.target.parentNode; // parent of the delete button
-  
-      bookModal.showModal();
-  
-      const confirmBtn = document.querySelector(".confirm-btn");
-      const cancelBtn = document.querySelector(".cancel-btn");
-  
-      confirmBtn.addEventListener("click", function () {
-        const objContainer = document.querySelector(".objContainer");
-        objContainer.removeChild(bookObj);
-        bookModal.close();
+      deleteBtn.addEventListener("click", () => {
+        const confirmDialog = document.querySelector('.confirmation-dialog');
+        
+        confirmDialog.classList.add('show')
+        removeBookFromLibrary(index);
+        
+        setTimeout(() => {
+            confirmDialog.classList.remove('show')
+        }, 1500)
+
+        console.log(`Index ${index} succesfully deleted`);
       });
   
-      cancelBtn.addEventListener("click", function () {
-        bookModal.close();
-      });
-    }
-  });
-  
-  // loadFromLocalStorage();
-  displayBook();
-  
-  // Function to save myLibrary to local storage
-  function saveToLocalStorage() {
-    localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+      bookObj.appendChild(p);
+      bookObj.appendChild(deleteBtn);
+      objContainer.appendChild(bookObj);
+      console.log(myLibrary);
+    });
   }
   
-  // Function to load myLibrary from local storage
-  function loadFromLocalStorage() {
-    const storedLibrary = localStorage.getItem("myLibrary");
-  
-    if (storedLibrary) {
-      myLibrary = JSON.parse(storedLibrary);
-    }
+  // remove book from array
+  function removeBookFromLibrary(index) {
+    myLibrary.splice(index, 1);
+    console.log(myLibrary);
+    renderLibrary();
   }
   
-  // Modal Button functionalities
-  const modal = document.querySelector(".modal");
-  const addBookBtn = document
-    .querySelector(".add-book-btn")
-    .addEventListener("click", function () {
-      modal.showModal();
-    });
-  
-  const closeModalBtn = document
-    .querySelector(".close-modal")
-    .addEventListener("click", function () {
-      modal.close();
-    });
+  // initialize the library array on DOM load
+  renderLibrary();
   
